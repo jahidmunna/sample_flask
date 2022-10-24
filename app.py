de71d493e730ef4785e7e9270947c42cd7ff4259
jahidmunna <jimunna150@gmail.com>
2022-10-25 01:12:11 +0900

    added logout option

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, redirect
 
 from api import api
 from authentication import auth
@@ -23,6 +23,15 @@ def api_call(user_input):
     }
     return jsonify(response)
 
+@app.route('/logged_out')
+def Logout():
+     return redirect("http://random_user:random_password@127.0.0.1:5000/logout")
+
+@app.route('/logout')
+def logout_page():
+    return '''
+        Logged Out! <br>Click <a href='/'> HERE</a> to go to the home page 
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True)
